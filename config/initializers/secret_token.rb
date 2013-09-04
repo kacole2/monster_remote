9c265f18fc23d49609a65fcc30c024f452b63452
Kendrick Coleman <kendrickcoleman@gmail.com>
2013-09-04 11:34:40 -0400

    Initial Commit
    Prototype without scaffolding for settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monster::Application.config.secret_key_base = '38fb53146dff4bb5b6cab78916792748eccc93db4bff81ad94cf810969c847b35e9b5dc407def486610f299294703f7d1bec745323c76b0c7df72b30d5491dea'
