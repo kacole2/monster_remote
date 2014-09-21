8ad31df2b1315105857407fc321f3392fda4421d
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-09-21 12:01:39 -0400

    modal attempt 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,6 @@ $(document).ready(function(){
 	window.setInterval(function () {
         $("#FlashModal").modal("hide");
     }, 15000); 
-    	
-	$('#FlashModal').on('hidden.bs.modal', function () {
-    	window.location = window.location.pathname;
-	})
 	
 	jQuery(document).ready(function() {
 		sec = 15;
@@ -36,6 +32,44 @@ $(document).ready(function(){
 	}); 
 	
 });
+
+function adjustModalMaxHeightAndPosition(){
+  $('.modal').each(function(){
+    if($(this).hasClass('in') === false){
+      $(this).show();
+    }
+    var contentHeight = $(window).height() - 60;
+    var headerHeight = $(this).find('.modal-header').outerHeight() || 2;
+    var footerHeight = $(this).find('.modal-footer').outerHeight() || 2;
+
+    $(this).find('.modal-content').css({
+      'max-height': function () {
+        return contentHeight;
+      }
+    });
+
+    $(this).find('.modal-body').css({
+      'max-height': function () {
+        return contentHeight - (headerHeight + footerHeight);
+      }
+    });
+
+    $(this).find('.modal-dialog').addClass('modal-dialog-center').css({
+      'margin-top': function () {
+        return -($(this).outerHeight() / 2);
+      },
+      'margin-left': function () {
+        return -($(this).outerWidth() / 2);
+      }
+    });
+    if($(this).hasClass('in') === false){
+      $(this).hide();
+    }
+  });
+}
+if ($(window).height() >= 320){
+  $(window).resize(adjustModalMaxHeightAndPosition).trigger("resize");
+}
 
 window.setTimeout(function() {
 	$("#notice").fadeTo(500, 0).slideUp(500, function(){
